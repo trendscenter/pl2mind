32aa0e2e5d863448c3ddc2a1f224ddb0e009222a
rdevon <devon@cs.unm.edu>
2014-11-21 12:47:39 -0700

    Added NICE tutorial files.

---
+++ tutorials/nice/train_nice_smri.py
@@ -0,0 +1,46 @@
+"""
+Module to train VAE on sMRI.
+"""
+
+import logging
+import numpy as np
+from os import path
+from pylearn2.config import yaml_parse
+from pylearn2.neuroimaging_utils.datasets import MRI
+from pylearn2.utils import serial
+from theano import config
+
+logging.basicConfig(format="[%(levelname)s]:%(message)s")
+
+def train_yaml(yaml_file):
+    train = yaml_parse.load(yaml_file)
+    train.main_loop()
+
+def train(yaml_file, save_path, nvis, vn, center):
+    yaml = open(yaml_file, "r").read()
+    hyperparams = {"nvis": nvis,
+                   "half_nvis": nvis // 2,
+                   "center": center,
+                   "vn": vn,
+                   "save_path": save_path
+                   }
+    yaml = yaml % hyperparams
+    train_yaml(yaml)
+
+def train_vae():
+    data_path = serial.preprocess("${PYLEARN2_NI_PATH}/smri")
+    mask_file = path.join(data_path, "mask.npy")
+    mask = np.load(mask_file)
+    input_dim = len(np.where(mask.flatten() == 1)[0].tolist())
+    del mask
+    
+    vn = False
+    center = True
+
+    p = path.abspath(path.dirname(__file__))
+    yaml_file = path.join(p, "nice_smri.yaml")
+    save_path = "/export/mialab/users/$USER/pylearn2_outs/"
+    train(yaml_file, save_path, input_dim, vn, center)
+
+if __name__ == "__main__":
+    train_vae()
